3aa72d26f01adf4428996016efbbd643e4909d0d
Brandon Green <jailhousejordans@gmail.com>
2019-09-23 18:07:10 -0500

    issues

--- front-end/src/Components/HomeScreenCases/HomeScreenCases.js
+++ front-end/src/Components/HomeScreenCases/HomeScreenCases.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 
+
+
+
+
 const HomeScreenCases = () => {
+    const mockData = {
+        name: 'mockName',
+        lastLocation: 'mockLocation',
+        currentCity: "mockCity",
+        familyMember: 'mockFamName',
+        relationship: 'Relationship',
+        famMemberLastLocation: 'mockFamMemberLastLocation',
+        volunteer: "mockVolunteer"
+    }
+    const [data, setData] = useState(mockData)
+
+    useEffect(() => {
+
+        axios
+            .get()
+            .then()
+
+    }, [])
+
     return (
         <div>
-            axios Get Request Data Here
+            <h1>homescreen</h1>
         </div>
     )
 }
+
+
+
 
 export default HomeScreenCases;
