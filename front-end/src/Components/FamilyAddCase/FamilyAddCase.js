db29f56ae96c32651342fa028ee32355eb1fbc0b
irisjitomo <irisjitomo@eyahoo.com>
2019-09-23 13:35:05 -0400

    added volunteer edit and delete case form

--- front-end/src/Components/FamilyAddCase/FamilyAddCase.js
+++ front-end/src/Components/FamilyAddCase/FamilyAddCase.js
@@ -17,15 +17,15 @@ const FamilyAddCase = props => {
             Add New Case Form:
             </h1>
             <form>
-                <input name='name'/>
-                <input name='lastLocation' />
-                <input name='currentCity' />
+                <input name='name' placeholder='Name of missing or homeless person'/>
+                <input name='lastLocation' placeholder='Last known location' />
+                <input name='currentCity' placeholder='Current city of last known location'/>
                 <h3>For family members</h3>
-                <input name='familyMemberName'/>
-                <input name='relationship'/>
-                <input name='familyMemberLastLocation' />
+                <input name='familyMemberName' placeholder='Name of family member posting'/>
+                <input name='relationship' placeholder="Relationship to person"/>
+                <input name='familyMemberLastLocation' placeholder="Family member location"/>
                 <h3>For Volunteer</h3>
-                <input name ='volunteerName' />
+                <input name ='volunteerName' placeholder="Volunteer name"/>
             </form>
         </div>
     )
---
+++ front-end/src/Components/VoluneteerEditandDelete.js/VolunteerEditandDelete.js
@@ -0,0 +1,57 @@
+import React, {useState, useEffect} from 'react';
+
+import axios from 'axios';
+
+
+const VolunteerEditandDelete = props => {
+
+    const initialCaseDetails = {
+        id: `${props.match.params.id}`,
+        name: '',
+        lastLocation: '',
+        currentCity: '',
+        familyMemberName: '',
+        relationship: '',
+        famMemberLastLocation : '',
+        volunteer: ''
+    }
+
+    const [detail, setDetail] = useState(initialCaseDetails);
+
+    const handleChange = e => {
+        e.persist();
+        setDetail({
+            ...detail,
+            [e.target.name] : e.target.value
+        })
+    }
+    const submit = e => {
+        e.preventDefault();
+        // axios PUT request and then props.history.push to '/home-volunteer'        
+    }
+    const deleteCase = e => {
+        e.preventDefault();
+        // axios DELETE request and then props.history.push to '/home-volunteer'
+    }
+    
+
+    return (
+        <div>
+            <form onSubmit={submit}>
+                <input name='name' value={detail.name}/>
+                <input name='lastLocation' value={detail.lastLocation}/>
+                <input name='currentCity' value={detail.currentCity}/>
+                <h3>For family members</h3>
+                <input name='familyMemberName' value={detail.familyMemberName}/>
+                <input name='relationship' value={detail.relationship}/>
+                <input name='famMemberLastLocation' value={detail.famMemberLastLocation}/>
+                <h3>For Volunteer</h3>
+                <input name ='volunteer' value={detail.volunteer}/>
+                <button>Save Edit</button>
+                <button onClick={deleteCase}>Delete Case Form</button>
+            </form>
+        </div>
+    )
+}
+
+export default VolunteerEditandDelete;
