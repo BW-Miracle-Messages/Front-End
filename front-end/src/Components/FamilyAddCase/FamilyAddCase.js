d9001315da3f99d099adb99f9ed2e36277ea5042
irisjitomo <irisjitomo@eyahoo.com>
2019-09-23 12:49:51 -0400

    made volunteer and family case forms

--- front-end/src/Components/FamilyAddCase/FamilyAddCase.js
+++ front-end/src/Components/FamilyAddCase/FamilyAddCase.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 const FamilyAddCase = props => {
 
+    const handleItems = e => { 
+
+    }
+
+    const submitForm = e => {
+        e.preventDefault();
+    }
 
     return (
         <div>
-            <h1>Add Family Case Form Here</h1>
+             <h1>
+            Add New Case Form:
+            </h1>
+            <form>
+                <input name='name'/>
+                <input name='lastLocation' />
+                <input name='currentCity' />
+                <h3>For family members</h3>
+                <input name='familyMemberName'/>
+                <input name='relationship'/>
+                <input name='familyMemberLastLocation' />
+                <h3>For Volunteer</h3>
+                <input name ='volunteerName' />
+            </form>
         </div>
     )
 }
--- front-end/src/Components/VolunteerAddCase/VolunteerAddCase.js
+++ front-end/src/Components/VolunteerAddCase/VolunteerAddCase.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const VolunteerAddCase = () => {
+
+    const handleItems = e => { 
+
+    }
+
+    const submitForm = e => {
+        e.preventDefault();
+    }
+
     return (
         <div>
-            Add Volunteer Form
+            <h1>
+            Add New Case Form:
+            </h1>
+            <form>
+                <input name='name'/>
+                <input name='lastLocation' />
+                <input name='currentCity' />
+                <h3>For family members</h3>
+                <input name='familyMemberName'/>
+                <input name='relationship'/>
+                <input name='familyMemberLastLocation' />
+                <h3>For Volunteer</h3>
+                <input name ='volunteerName' />
+            </form>
         </div>
     )
 }
