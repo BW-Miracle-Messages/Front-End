bb7732f49700baa8bba8c99becba840b12051fbb
Martins-O-U <martinsonyedikachi@gmail.com>
2019-09-27 10:35:09 +0100

    Cleaned out a few commented/Unused codes

--- front-end/src/Components/HomeScreenCases/HomeScreenCases.js
+++ front-end/src/Components/HomeScreenCases/HomeScreenCases.js
@@ -28,29 +28,5 @@ const HomeScreenCases = props => {
     );
 }
 
-    // CaseCard
-
-//  function CaseCard({data}, props) {
-    
-//     const goToEdit = () => {
-//         props.history.push(`/volunteer-edit-delete-case/${props.match.params.id}`)
-//         console.log(CaseCard)
-//     }
-//     return (
-//         <div className='case-card' key={data.id}>
-//             <h4><i>{data.homie_name}</i></h4>
-//             <h6>Current State: <i>{data.current_state}</i></h6>
-//             <h6>Current City: <i>{data.current_city}</i></h6>
-//             <h6>Last Location: <i>{data.last_location}</i></h6>
-//             <div className='btn-edit'>
-//                 <button className='button-edit' onClick={goToEdit}>Edit Case</button>
-//                 <button className='button-edit'>Add Family Data</button>
-//             </div>
-
-//         </div>
-//     );
-//   }
-
-
 export default HomeScreenCases;
 
