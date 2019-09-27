bf8b183aced244ca8da653471bc1795283f42f1e
Brandon Green <jailhousejordans@gmail.com>
2019-09-27 10:33:33 -0500

    fixed errors

--- front-end/src/Components/HomeScreenCases/HomeScreenCases.js
+++ front-end/src/Components/HomeScreenCases/HomeScreenCases.js
@@ -28,22 +28,5 @@ const HomeScreenCases = props => {
     );
 }
 
-function CaseCard({ data }) {
-    return (
-        <div className='case-card'>
-            <h4><i>{data.homie_name}</i></h4>
-            <h6>Current State: <i>{data.current_state}</i></h6>
-            <h6>Current City: <i>{data.current_city}</i></h6>
-            <h6>Last Location: <i>{data.last_location}</i></h6>
-            <div className='btn-edit'>
-                <button className='button-edit'>Edit Case</button>
-                <button className='button-edit'>Add Family Data</button>
-            </div>
-
-        </div>
-    );
-}
-
-
 export default HomeScreenCases;
 
